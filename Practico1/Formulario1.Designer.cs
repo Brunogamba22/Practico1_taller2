cae1cf05a483b36d750dbb28f50160532f30f85a
JonathanGarcia <martingarciayoni@gmail.com>
2025-08-18 13:39:08 -0300

    Realizacion de puntos 3, 4, y 5

--- Practico1/Formulario1.Designer.cs
+++ Practico1/Formulario1.Designer.cs
@@ -28,20 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.BGuardar = new System.Windows.Forms.Button();
+            this.BEliminar = new System.Windows.Forms.Button();
+            this.LApellido = new System.Windows.Forms.Label();
+            this.LNombre = new System.Windows.Forms.Label();
+            this.textBoxApellido = new System.Windows.Forms.TextBox();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.textBoxMultiL = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // BGuardar
+            // 
+            this.BGuardar.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.BGuardar.Location = new System.Drawing.Point(30, 157);
+            this.BGuardar.Name = "BGuardar";
+            this.BGuardar.Size = new System.Drawing.Size(75, 23);
+            this.BGuardar.TabIndex = 0;
+            this.BGuardar.Text = "Guardar";
+            this.BGuardar.UseVisualStyleBackColor = false;
+            this.BGuardar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BEliminar
+            // 
+            this.BEliminar.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.BEliminar.Location = new System.Drawing.Point(111, 157);
+            this.BEliminar.Name = "BEliminar";
+            this.BEliminar.Size = new System.Drawing.Size(75, 23);
+            this.BEliminar.TabIndex = 1;
+            this.BEliminar.Text = "Eliminar";
+            this.BEliminar.UseVisualStyleBackColor = false;
+            this.BEliminar.Click += new System.EventHandler(this.BEliminar_Click);
+            // 
+            // LApellido
+            // 
+            this.LApellido.AutoSize = true;
+            this.LApellido.Location = new System.Drawing.Point(36, 68);
+            this.LApellido.Name = "LApellido";
+            this.LApellido.Size = new System.Drawing.Size(44, 13);
+            this.LApellido.TabIndex = 2;
+            this.LApellido.Text = "Apellido";
+            // 
+            // LNombre
+            // 
+            this.LNombre.AutoSize = true;
+            this.LNombre.Location = new System.Drawing.Point(36, 105);
+            this.LNombre.Name = "LNombre";
+            this.LNombre.Size = new System.Drawing.Size(44, 13);
+            this.LNombre.TabIndex = 3;
+            this.LNombre.Text = "Nombre";
+            // 
+            // textBoxApellido
+            // 
+            this.textBoxApellido.Location = new System.Drawing.Point(124, 61);
+            this.textBoxApellido.Name = "textBoxApellido";
+            this.textBoxApellido.Size = new System.Drawing.Size(100, 20);
+            this.textBoxApellido.TabIndex = 4;
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.Location = new System.Drawing.Point(124, 102);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNombre.TabIndex = 5;
+            // 
+            // textBoxMultiL
+            // 
+            this.textBoxMultiL.Location = new System.Drawing.Point(288, 65);
+            this.textBoxMultiL.Multiline = true;
+            this.textBoxMultiL.Name = "textBoxMultiL";
+            this.textBoxMultiL.Size = new System.Drawing.Size(165, 154);
+            this.textBoxMultiL.TabIndex = 6;
+            this.textBoxMultiL.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // MiPrimerFroms
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 261);
+            this.ClientSize = new System.Drawing.Size(481, 261);
+            this.Controls.Add(this.textBoxMultiL);
+            this.Controls.Add(this.textBoxNombre);
+            this.Controls.Add(this.textBoxApellido);
+            this.Controls.Add(this.LNombre);
+            this.Controls.Add(this.LApellido);
+            this.Controls.Add(this.BEliminar);
+            this.Controls.Add(this.BGuardar);
             this.Name = "MiPrimerFroms";
             this.Text = "Mi Primer Froms";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BGuardar;
+        private System.Windows.Forms.Button BEliminar;
+        private System.Windows.Forms.Label LApellido;
+        private System.Windows.Forms.Label LNombre;
+        private System.Windows.Forms.TextBox textBoxApellido;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.TextBox textBoxMultiL;
     }
 }
 
--- Practico1/Formulario1.cs
+++ Practico1/Formulario1.cs
@@ -16,5 +16,20 @@ namespace Practico1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BEliminar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
