0eef4cb0c21417821a8f0e693972754094ca9953
Brunogamba22 <brunogamba439@gmail.com>
2025-08-25 09:15:50 -0300

    solucion de problemas con las carpetas

--- Practico4/FormularioTP4.cs
+++ Practico4/FormularioTP4.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Practico4
 {
-    public partial class Form1 : Form
+    public partial class FormularioTP4 : Form
     {
-        public Form1()
+        public FormularioTP4()
         {
             InitializeComponent();
         }
